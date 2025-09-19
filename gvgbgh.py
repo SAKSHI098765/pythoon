37da2aa67ffc888036bb8cc172347187a541fb34
SAKSHI098765 <sakshi_259048@saitm.ac.in>
2025-09-19 10:09:33 +0530

    Add files via upload

---
+++ gvgbgh.py
@@ -0,0 +1,14 @@
+def calculator(a,b,operator):
+    if operator =="+":
+        print (a+b)
+    elif c=="*":
+        print (a*b)
+    elif c=="/":
+        print (a/b)
+    elif c== "*":
+        print (a*b)
+
+a=int(input("Enter the first no."))
+b=int(input("Enter the second no."))
+c==input("enter your choise + , - , *, /")
+calculation(a,b,c)
